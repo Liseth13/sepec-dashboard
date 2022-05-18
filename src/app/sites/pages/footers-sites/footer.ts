7a287e89ab14934ae85ac1b669b3534717ac5240
Liseth J <jimenezliseth54@gmail.com>
2022-05-18 18:34:10 -0500

    se adelanto el Crud

---
+++ src/app/sites/pages/footers-sites/footer.ts
@@ -0,0 +1,16 @@
+export class Footer {
+    public sitio = 1;
+
+    public email = '';
+
+    public address = '';
+
+    public phone = '';
+
+    public border = false;
+
+    public sectionTaskType = '';
+
+    public status = false;
+
+}
--- src/app/sites/pages/footers-sites/footers-sites.component.ts
+++ src/app/sites/pages/footers-sites/footers-sites.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Footer } from './footer';
 import Swal from 'sweetalert2';
 import { footerSiteService } from '../../services/footers-sites.service';
 
@@ -12,35 +13,47 @@ import { footerSiteService } from '../../services/footers-sites.service';
   styleUrls: ['./footers-sites.component.scss']
 })
 export class FootersSitesComponent implements OnInit {
+
+  selectedST: Footer | undefined = Object.create(null);
+  titleTaskSection = '';
+
+
   public footers  : Array <any> = [];
   public charging : boolean = true;
-  private totalFooterSite:number = 0;
+  public totalFooterSite:number = 0;
   formFooterCreate: any;
   formFooterEdit: any;
   websiteId: any;
   closeResult: string;
+  count = 0;
+  sectionTask: Footer[] | null = null;
 
     //Paginación
     page =1;
     pageSize = 8;
     collectionSize = 0;
     _searchTerm = '';
+    webFooterService: any;
+    
 
   constructor(
-    private formBuilder : FormBuilder, private footerSiteService : footerSiteService ,
+    private formBuilder : FormBuilder, private footerSiteService : footerSiteService,
     private modalService: NgbModal
   ) { }
 
   ngOnInit(): void {
-   this.getWebFooters()
+   this.getWebFooters();
+   this.loadformSiteCreate();
+   console.log(this.selectedST);
+   
   }
 
   loadformSiteCreate(){
     this.formFooterCreate = this.formBuilder.group({ 
       sitio:['',[Validators.required, Validators.maxLength(20),Validators.minLength(3)]],
       email:['',[Validators.required, Validators.maxLength(40),Validators.minLength(8)]],
-      direccion:['',[Validators.required, Validators.maxLength(40),Validators.minLength(5)]],
-      telefono:['',[Validators.required, Validators.maxLength(40),Validators.minLength(8)]],
+      address:['',[Validators.required, Validators.maxLength(40),Validators.minLength(5)]],
+      phone:['',[Validators.required, Validators.maxLength(40),Validators.minLength(8)]],
     })
   }
 
@@ -48,8 +61,8 @@ export class FootersSitesComponent implements OnInit {
     this.formFooterEdit = this.formBuilder.group({
       sitio:['',[Validators.required, Validators.maxLength(20),Validators.minLength(3)]],
       email:['',[Validators.required, Validators.maxLength(40),Validators.minLength(8)]],
-      direccion:['',[Validators.required, Validators.maxLength(40),Validators.minLength(8)]],
-      telefono:['',[Validators.required, Validators.maxLength(40),Validators.minLength(8)]],
+      address:['',[Validators.required, Validators.maxLength(40),Validators.minLength(8)]],
+      phone:['',[Validators.required, Validators.maxLength(40),Validators.minLength(8)]],
       is_active:[null,[Validators.required,]],
     })
   }
@@ -90,8 +103,8 @@ export class FootersSitesComponent implements OnInit {
       this.formFooterEdit.patchValue({
         sitio: data.sitio,
         email: data.email,
-        direccion: data.direccion,
-        telefono: data.telefono,
+        address: data.address,
+        phone: data.phone,
         is_active: data.is_active
       });
 
@@ -120,5 +133,53 @@ export class FootersSitesComponent implements OnInit {
 			return `with: ${reason}`;
 		}
   }
+
+  addTaskSection() {
+
+    this.formFooterCreate.reset();
+
+    if ((document.getElementById('rightMenu')as HTMLFormElement).style.width === '300px') {
+        this.closeRightMenu();
+        return;
+    }
+    (document.getElementById('rightMenu')as HTMLFormElement).style.width = '300px';
+
+    this.titleTaskSection = 'Task';
+
+    const footer = new Footer();
+    footer.email = 'bla@bla.com';
+    footer.address = 'bla@bla.com';
+    footer.phone = 'bla@bla.com';
+    footer.status = true;
+
+  }
+
+
+  closeRightMenu() {
+    (document.getElementById('rightMenu')as HTMLFormElement).style.width = '0';
+  }
+
+  create(){
+      
+    console.log (this.formFooterCreate.value)
+    if(this.formFooterCreate.invalid){
+      Swal.fire('Error','Faltan Campos Por Validar','warning')
+    }else{
+      
+      this.webFooterService.createWebSite(this.formFooterCreate.value).subscribe(
+        (res:any)=>{
+          console.log(res)
+          
+          this.getWebFooters()
+          this.closeModal()
+          Swal.fire(' Success','Guardado correctamente','success')
+        },
+        (error:any)=>{
+          console.log(error)
+          Swal.fire('Error','vuelva a intentarlo','error')
+        }
+      )
+    }
+}
 }
 
